c2cb4e163780b6ec154463ef146108e8a2731727
sobu95 <damiankalka.sobu@gmail.com>
2025-08-05 14:12:36 +0200

    Handle lost MySQL connections

--- config/database.php
+++ config/database.php
@@ -5,6 +5,7 @@ class Database {
     private $username;
     private $password;
     private $conn;
+    private $options;
 
     public function __construct() {
         $config = include __DIR__ . '/config.php';
@@ -12,18 +13,37 @@ class Database {
         $this->db_name = $config['db_name'];
         $this->username = $config['db_username'];
         $this->password = $config['db_password'];
+        $this->options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_PERSISTENT => true
+        ];
     }
 
-    public function getConnection() {
-        $this->conn = null;
+    private function connect() {
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password,
+                $this->options
+            );
             $this->conn->exec("SET NAMES utf8");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $exception) {
             throw new Exception("Błąd połączenia z bazą danych: " . $exception->getMessage());
         }
+    }
+
+    public function getConnection() {
+        if ($this->conn === null) {
+            $this->connect();
+        }
+        return $this->conn;
+    }
+
+    public function reconnect() {
+        $this->conn = null;
+        $this->connect();
         return $this->conn;
     }
 }
